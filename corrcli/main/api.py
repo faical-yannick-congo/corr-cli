9b350a92cf857a99f4ab1022c0f0fb0b399c8789
Faical Yannick Palingwende Congo <faical.congo@nist.gov>
2017-06-05 16:31:57 -0400

    Few features added.

--- corrcli/main/api.py
+++ corrcli/main/api.py
@@ -2,17 +2,34 @@ import httplib
 import json
 import traceback
 import requests
+import ssl as sslAgent
+from io import BytesIO
+
+def conn_generator(host_full, port):
+    blocks = host_full.split("://")
+    host = blocks[1]
+    ssl = True if "https" == blocks[0] else False
+    try:
+        if ssl:
+            conn = httplib.HTTPSConnection(host, int(port), context=sslAgent._create_unverified_context())
+        else:
+            conn = httplib.HTTPConnection(host, int(port))
+        return conn
+    except:
+        traceback.print_exc()
+        return None
 
 def api_status(config=None, host=None, port=None):
     if host is None or port is None:
-        try:
-            conn = httplib.HTTPConnection(config['api']['host'], int(config['api']['port']))
-        except:
+        conn = conn_generator(config['api']['host'], config['api']['port'])
+        if conn is None:
             return False
     else:
-        conn = httplib.HTTPConnection(host, int(port))
+        conn = conn_generator(host, port)
+        if conn is None:
+            return False
 
-    conn.request("GET", "/api/v0.1/public/api/status")
+    conn.request("GET", "/corr/api/v0.1/public/api/status")
     response = conn.getresponse()
     data = response.read()
     conn.close()
@@ -23,12 +40,13 @@ def api_status(config=None, host=None, port=None):
         else:
             return False
     except:
+        traceback.print_exc()
         return False
 
 def project_create(config=None, name=None, description='', goals='',
                    tags=[], access='private', group='computational'):
     if api_status(config=config):
-        conn = httplib.HTTPConnection(config['api']['host'], int(config['api']['port']))
+        conn = conn_generator(config['api']['host'], config['api']['port'])
     else:
         return [False, 'No configured api.']
 
@@ -42,7 +60,7 @@ def project_create(config=None, name=None, description='', goals='',
     request['group'] = group
     conn.request(
         "POST", 
-           "/api/v0.1/private/{0}/no-app/project/create".format(config['api']['key']), 
+           "/corr/api/v0.1/private/{0}/no-app/project/create".format(config['api']['key']), 
            json.dumps(request), headers)
     response = conn.getresponse()
     data = response.read()
@@ -62,7 +80,7 @@ def project_create(config=None, name=None, description='', goals='',
 def project_update(config=None, project=None, description=None, goals=None,
                    tags=None, access=None, group=None):
     if api_status(config=config):
-        conn = httplib.HTTPConnection(config['api']['host'], int(config['api']['port']))
+        conn = conn_generator(config['api']['host'], config['api']['port'])
     else:
         return [False, 'No configured api.']
         
@@ -80,7 +98,7 @@ def project_update(config=None, project=None, description=None, goals=None,
         request['group'] = group
     conn.request(
         "POST", 
-           "/api/v0.1/private/{0}/no-app/project/update/{1}".format(config['api']['key'], project), 
+           "/corr/api/v0.1/private/{0}/no-app/project/update/{1}".format(config['api']['key'], project), 
            json.dumps(request), headers)
     response = conn.getresponse()
     data = response.read()
@@ -97,11 +115,11 @@ def project_update(config=None, project=None, description=None, goals=None,
 
 def project_all(config=None):
     if api_status(config=config):
-        conn = httplib.HTTPConnection(config['api']['host'], int(config['api']['port']))
+        conn = conn_generator(config['api']['host'], config['api']['port'])
     else:
         return [False, 'No configured api.']
 
-    conn.request("GET", "/api/v0.1/private/{0}/no-app/projects".format(config['api']['key']))
+    conn.request("GET", "/corr/api/v0.1/private/{0}/no-app/projects".format(config['api']['key']))
     response = conn.getresponse()
     data = response.read()
     conn.close()
@@ -117,14 +135,14 @@ def project_all(config=None):
 
 def record_create(config=None, project=None, request={}):
     if api_status(config=config):
-        conn = httplib.HTTPConnection(config['api']['host'], int(config['api']['port']))
+        conn = conn_generator(config['api']['host'], config['api']['port'])
     else:
         return [False, 'No configured api.']
 
     headers = {"Accept": "application/json"}
     conn.request(
         "POST",
-        "/api/v0.1/private/{0}/no-app/project/record/create/{1}".format(
+        "/corr/api/v0.1/private/{0}/no-app/project/record/create/{1}".format(
             config['api']['key'], project), json.dumps(request), headers)
     response = conn.getresponse()
     data = response.read()
@@ -140,14 +158,14 @@ def record_create(config=None, project=None, request={}):
 
 def record_update(config=None, record=None, request={}):
     if api_status(config=config):
-        conn = httplib.HTTPConnection(config['api']['host'], int(config['api']['port']))
+        conn = conn_generator(config['api']['host'], config['api']['port'])
     else:
         return [False, 'No configured api.']
 
     headers = {"Accept": "application/json"}
     conn.request(
         "POST",
-        "/api/v0.1/private/{0}/no-app/record/update/{1}".format(
+        "/corr/api/v0.1/private/{0}/no-app/record/update/{1}".format(
             config['api']['key'], record), json.dumps(request), headers)
     response = conn.getresponse()
     data = response.read()
@@ -164,7 +182,7 @@ def record_update(config=None, record=None, request={}):
 def upload_file(config=None, path=None, obj=None, group=None):
     if api_status(config=config):
         if path and group and obj:
-            url = "http://{0}:{1}/api/v0.1/private/{2}/no-app/file/upload/{3}/{4}".format(
+            url = "{0}:{1}/corr/api/v0.1/private/{2}/no-app/file/upload/{3}/{4}".format(
                 config['api']['host'], int(config['api']['port']),
                 config['api']['key'], group, obj)
         else:
@@ -175,7 +193,7 @@ def upload_file(config=None, path=None, obj=None, group=None):
     
     # print path
     files = {'file': open('{0}'.format(path))}
-    response = requests.post(url, files=files)
+    response = requests.post(url, files=files, verify=False)
     data = response.content
 
     try:
@@ -190,7 +208,7 @@ def upload_file(config=None, path=None, obj=None, group=None):
 
 def project_env_next(config=None, project=None, path=None):
     if api_status(config=config):
-        conn = httplib.HTTPConnection(config['api']['host'], int(config['api']['port']))
+        conn = conn_generator(config['api']['host'], config['api']['port'])
     else:
         return [False, 'No configured api.']
 
@@ -204,7 +222,7 @@ def project_env_next(config=None, project=None, path=None):
     }
     conn.request(
         "POST",
-        "/api/v0.1/private/{0}/no-app/project/env/next/{1}".format(
+        "/corr/api/v0.1/private/{0}/no-app/project/env/next/{1}".format(
             config['api']['key'], project), json.dumps(request), headers)
     response = conn.getresponse()
     data = response.read()
@@ -224,5 +242,42 @@ def project_env_next(config=None, project=None, path=None):
     except:
         return [False, str(traceback.print_exc())]
 
+def project_records(config=None, project_id=None):
+    if api_status(config=config):
+        conn = conn_generator(config['api']['host'], config['api']['port'])
+    else:
+        return [False, 'No configured api.']
 
+    conn.request("GET", "/corr/api/v0.1/private/{0}/no-app/project/records/{1}".format(config['api']['key'], project_id))
+    response = conn.getresponse()
+    data = response.read()
+    conn.close()
+    try:
+        data_json = json.loads(data)
+        # print data_json
+        if data_json['code'] == 200:
+            return [True, data_json['content']]
+        else:
+            return [False, data_json['content']]
+    except:
+        return [False, str(traceback.print_exc())]
 
+def record_download(config=None, record_id=None):
+    if api_status(config=config):
+        if record_id:
+            url = "{0}:{1}/corr/api/v0.1/private/{2}/no-app/record/download/{3}".format(
+                config['api']['host'], int(config['api']['port']),
+                config['api']['key'], record_id)
+        else:
+            return [False, 'Required record_id.']
+    else:
+        return [False, 'No configured api.']
+    
+    try:
+        response = requests.get(url, verify=False)
+        file_buffer = BytesIO(response.content)
+        file_buffer.seek(0)
+        return file_buffer
+    except:
+        print(traceback.print_exc())
+        return None
